2e160931746b95ab6cf0f225ad30144d75f64b04
OGHENEMINE <mineoghorodi@gmail.com>
2024-02-29 13:37:34 +0100

    update main.jsx file

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import ActiveTabContextProvider from "./contexts/ActiveTabContext.jsx";
-import ModeProvider from "./contexts/ModeContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ActiveTabContextProvider>
-      <ModeProvider>
-        <App />
-      </ModeProvider>
+      <App />
     </ActiveTabContextProvider>
   </React.StrictMode>
 );
