6f47b7cfb0d42baae922325fc64eaac29d22d794
OGHENEMINE <mineoghorodi@gmail.com>
2024-02-29 12:58:05 +0100

    update the Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,7 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext, useState } from "react";
 import { TabContext } from "../contexts/ActiveTabContext";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMoon, faSun } from "@fortawesome/free-regular-svg-icons";
 import { ModeContext } from "../contexts/ModeContext";
 
 function Navbar() {
@@ -20,14 +19,6 @@ function Navbar() {
     <header>
       <div className="logo">
         <div>LOGO.</div>
-        <div className="mode-toggle">
-          <div className={mode === "dark"? "active" : ""} onClick={handleSetMode}>
-            <FontAwesomeIcon icon={faSun}/>
-          </div>
-          <div className={mode === "light"? "active" : ""} onClick={handleSetMode}>
-            <FontAwesomeIcon icon={faMoon}/>
-          </div>
-        </div>
       </div>
       <nav className={mobileNav ? "active" : ""}>
         {listNav.map((item, index) => (
